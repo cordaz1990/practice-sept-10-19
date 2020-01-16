ca65de000cad7ce9078c3dd595e853cdea53bee9
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 23:04:22 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,17 @@
-print('hello')
-print('world')
-print('hello')
+def subtract(d1, d2):
+    res = dict()
+    for key in d1:
+      if key not in d2:
+        res[key] = None
+     
+    return res
+  
+  
+  def has_duplicates(t):
+      d = {}
+      for x in t:
+        if x in d:
+          return True
+        d[x] = True
+        
+      return False
